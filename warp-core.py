1328ac436c7d09606339d9fb4db12919070d976f
Henry Berg <hberg32@gmail.com>
2025-07-13 21:02:13 -0400

    various changes

--- warp-core.py
+++ warp-core.py
@@ -5,32 +5,43 @@ import requests
 
 from pycycling.cycling_power_service import CyclingPowerService
 
-async def run(trainer_address, warp_core_address, core_running):
+core_running=False
+
+async def run(trainer_address, warp_core_address):
     async with BleakClient(trainer_address) as client:
         def my_measurement_handler(data):
+            global core_running
             speed = data.instantaneous_power
+
+            speed = int(speed/500*255)
 
             segments = []
             segments.append({"on": True})
-            for segment in range(1,8):
+
+            for segment in range(1,10):
                 if speed > 0:
                     if not core_running:
                         segments.append({"on": True, "sx":speed})
                     else:
                         segments.append({"sx":speed})
                 else:
-                    core_running = False
                     segments.append({"on": False})
 
-            command = {"seg":segments}
+            if speed > 0:
+                if not core_running:
+                    core_running = True
+            else:
+                core_running = False
 
-            requests.post("http://wled-48fc18.local/json/state", json=command)
+            command = {"seg":segments}
+            requests.post(f"http://{warp_core_address}/json/state", json=command)
 
         await client.is_connected()
         trainer = CyclingPowerService(client)
         trainer.set_cycling_power_measurement_handler(my_measurement_handler)
         await trainer.enable_cycling_power_measurement_notifications()
-#        await asyncio.sleep(120.0)
+        while True:
+            await asyncio.sleep(120)
 #        await trainer.disable_cycling_power_measurement_notifications()
 
 
@@ -41,5 +52,6 @@ if __name__ == "__main__":
 
     trainer_address = "CF:1D:0F:D9:7C:6D"
     warp_core_address = "wled-48fc18.local"
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(trainer_address, warp_core_address, False))
+    loop.run_until_complete(run(trainer_address, warp_core_address))
